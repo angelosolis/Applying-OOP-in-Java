a508867864dc680889c1c815a6696c68d94e0a98
Angelo Solis <88774848+angelosolis@users.noreply.github.com>
2022-10-15 13:52:36 +0800

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add extends Semi
+    {
+        public Add(int num1, int num2)
+        {
+            super(num1,num2);
+            toString();
+        }
+
+    @Override
+        public String toString()
+        {
+        return "The sum is ";
+        }
+    }
